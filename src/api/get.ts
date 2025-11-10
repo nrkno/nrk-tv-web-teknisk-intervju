e4bbe9d3c9d53fa1d97372ebb4e5eced725dfbb3
ingvilddev <72200992+ingvilddev@users.noreply.github.com>
2025-11-10 09:08:23 +0100

    Adding a proxy

--- src/api/get.ts
+++ src/api/get.ts
@@ -2,7 +2,7 @@ import { headlinersAPIResponse } from './data';
 import { Headliner } from './types';
 
 /**
- * API endpoint: https://psapi.nrk.no/tv/headliners/default
+ * API endpoint: /api/tv/headliners/default
  * API documentation: https://psapi.nrk.no/documentation/redoc/headliners/
  */
 export function getHeadliners(): Headliner[] {
