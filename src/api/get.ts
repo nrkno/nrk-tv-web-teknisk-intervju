4346aae765dd8b35df0a1338b0a63c9b7114ad1f
ingvilddev <72200992+ingvilddev@users.noreply.github.com>
2025-11-10 09:22:02 +0100

    Adding backup for endpoint

--- src/api/get.ts
+++ src/api/get.ts
@@ -8,3 +8,23 @@ import { HeadlinersAPIResponse } from './types';
 export function getHeadliners(): HeadlinersAPIResponse {
   return headlinersAPIResponse;
 }
+
+/**
+ * Backup in case API is not reachable.
+ * Simulates with a small timeout for async behavior.
+ */
+export function simulateHeadlinersAPIEndpoint(): Promise<{
+  ok: boolean;
+  status: number;
+  json: () => Promise<HeadlinersAPIResponse>;
+}> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        ok: true,
+        status: 200,
+        json: async () => headlinersAPIResponse,
+      });
+    }, 250);
+  });
+}
